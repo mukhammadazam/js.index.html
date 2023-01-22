3d80afe0bc202bc423b0c6fde12ca3578566ef75
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-01-22 23:10:24 +0300

    jsga

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// DOM object model bu bizning htmml limiz
+// const bilan let ning farqi constga velu bergandan keyin uzgartirib bulmaydi letga esa buladi
+// console.log(document); documentning uzini chaqirsak htmlning hammasini chqirib beradi
+const p = document.getElementById("p1"); // p1 bizda textning id si p1 bizga p ga velu yani qiymat qilib beradi
+
+// p.innerHTML = '<span class="samething">sjhakfwihfskjsfvj</span>'; bunday qilsak ham buladi  "innerHTML"---->bergandan keyin taglar berishi mumkin
+p.textContent = "lerem lerem lerem lerem lerem"
+console.log(p); // endi text bizda jsdan chiqyapti
