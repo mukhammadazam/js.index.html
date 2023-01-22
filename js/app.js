df306dc58d7b5300089e810081024d7310026e21
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-01-22 23:11:33 +0300

    javascript

--- js/app.js
+++ js/app.js
@@ -4,5 +4,6 @@
 const p = document.getElementById("p1"); // p1 bizda textning id si p1 bizga p ga velu yani qiymat qilib beradi
 
 // p.innerHTML = '<span class="samething">sjhakfwihfskjsfvj</span>'; bunday qilsak ham buladi  "innerHTML"---->bergandan keyin taglar berishi mumkin
-p.textContent = "lerem lerem lerem lerem lerem"
+p.textContent = "lerem lerem lerem lerem lerem"; // "textContent faqat textlar uchun"
+p.className = 'new-class'; // textga class qushib beradi
 console.log(p); // endi text bizda jsdan chiqyapti
