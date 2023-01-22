1753c7394d8482fd60cb7389bab548dbc6268a23
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-01-23 00:05:36 +0300

    js bmi

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
+function calculateBmi(a, b) {
+    console.log(a + b);
+}
+calculateBmi(10, 12);
+
+
+
 // DOM object model bu bizning htmml limiz
 // const bilan let ning farqi constga velu bergandan keyin uzgartirib bulmaydi letga esa buladi
-// console.log(document); documentning uzini chaqirsak htmlning hammasini chqirib beradi
-const p = document.getElementById("p1"); // p1 bizda textning id si p1 bizga p ga velu yani qiymat qilib beradi
-
+//console.log(document); //documentning uzini chaqirsak htmlning hammasini chqirib beradi
+//const p = document.getElementById("p1"); // p1 bizda textning id si p1 bizga p ga velu yani qiymat qilib beradi
+//const button = document.querySelector("#clikc");
+//const input = document.querySelector(".myinput");
+//console.log(input.value);
 // p.innerHTML = '<span class="samething">sjhakfwihfskjsfvj</span>'; bunday qilsak ham buladi  "innerHTML"---->bergandan keyin taglar berishi mumkin
-p.textContent = "lerem lerem lerem lerem lerem"; // "textContent faqat textlar uchun"
-p.className = 'new-class'; // textga class qushib beradi
-console.log(p); // endi text bizda jsdan chiqyapti
+//p.textContent = "lerem lerem lerem lerem lerem"; // "textContent faqat textlar uchun"
+//p.className = 'new-class'; // textga class qushib beradi
+//console.log(p); // endi text bizda jsdan chiqyapti
+//console.log(button);
+//console.log(input);
